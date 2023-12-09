0da7fd62198ead69c557e67ffadf06f0177c21e5
BogdanEvstygneiv <bonhead@ukr.net>
2023-12-09 21:55:34 +0200

    asdas

---
+++ wa-system/webasyst/lib/config/app.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    'name'         => 'Webasyst',
+    'prefix'       => 'webasyst',
+    'version'      => '2.9.7',
+    'critical'     => '2.9.7',
+    'vendor'       => 'webasyst',
+    'csrf'         => true,
+    'header_items' => array(
+        'settings' => array(
+            'icon'   => 'img/wa-settings/settings.svg',
+            'name'   => 'Settings',  // _w('Settings')
+            'link'   => 'settings',
+            'rights' => 'backend'
+        ),
+    ),
+    'ui'           => '1.3,2.0'
+);
